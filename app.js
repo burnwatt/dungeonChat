da918a2a65b985382b1e74ee09bd31327205e2cc
Steven K. Terry <stkterry@gmail.com>
2019-07-08 15:51:03 -0400

    2nd commit

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+
+//  SOME CHANGE!!!
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
 const app = express();
 
 app.get("/", (req, res) => res.send("DungeonChat!"));
