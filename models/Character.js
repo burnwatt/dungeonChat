7a12e98a77febd6aeaca65529075a4dcb6ba2c42
Josh Burnwatt <burnwattj@gmail.com>
2019-07-10 16:46:23 -0500

    Add Character model

---
+++ models/Character.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CharacterSchema = new Schema({
+  campaign_id: {
+    type: Schema.Types.ObjectId,
+    ref: 'campaigns'
+  },
+  user_id: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  char_attrs: {
+    type: Object,
+    default: {}
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Character = mongoose.model('characters', CharacterSchema);
