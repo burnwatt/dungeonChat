3960a3733e7103e0781a34f87a86d9e2dc7c2699
Josh Burnwatt <burnwattj@gmail.com>
2019-07-10 10:16:59 -0500

    Update splash styling

--- frontend/src/components/splash/splash_page.jsx
+++ frontend/src/components/splash/splash_page.jsx
@@ -31,40 +31,51 @@ class SplashPage extends React.Component {
           </div>
         </div>
         <div className='splash-main'>
-          <div>
-            <div>
+            <div className='splash-primary'>
               <img
-                src="https://www.dictionary.com/e/wp-content/uploads/2019/06/1000x700-DnD-1.jpg"
+                className='primary-img'
+                src="https://static.wixstatic.com/media/1c4207_585a5631024c4d9ca265167ca286edb7~mv2_d_5100_3300_s_4_2.jpg/v1/fill/w_1119,h_724,al_c,q_90,usm_0.66_1.00_0.01/1c4207_585a5631024c4d9ca265167ca286edb7~mv2_d_5100_3300_s_4_2.webp"
                 width="300px"
                 alt="whatever"
               />
-              <div>
+              <div className='primary-body'>
                 <h2>A platform to play by chat</h2>
-                Tones, dice rolls, character sheets and more in a straight
-                forward chat application.
+                <p>
+                  Tones, dice rolls, character sheets and more in a straight
+                  forward chat application.
+                </p>
               </div>
             </div>
-            <div>
-              <div>
-                <div>
-                  <img
-                    src="https://www.dictionary.com/e/wp-content/uploads/2019/06/1000x700-DnD-1.jpg"
-                    width="300px"
-                    alt="whatever"
-                  />
-                  <div>Beginner Friendly Community</div>
-                  No experience with Role Playing? No problem. Find a public
-                  game with the "Beginner Friendly" tag and hop in!
+            <div className='secondary-wrapper'>
+              <div className='splash-secondary'>
+                <div className='secondary-body'>
+                  <h2>Beginner Friendly Community</h2>
+                  <p>
+                    No experience with Role Playing? No problem. Find a public
+                    game with the "Beginner Friendly" tag and hop in!
+                  </p>
                 </div>
-                <Link id='signup-btn' to={"/signup"}>
-                  <span>Join Dungeon Chat</span>
-                </Link>
+                <img
+                  className='secondary-img'
+                  src="https://ksr-ugc.imgix.net/assets/018/871/460/a8235dd1ee5921d2d618e7c8f19fd8ee_original.png?ixlib=rb-2.1.0&w=680&fit=max&v=1508727446&auto=format&gif-q=50&lossless=true&s=9caf0118c751b207e471e927ad659f24"
+                  width="300px"
+                  alt="whatever"
+                />
               </div>
+              <Link id='join-btn' to={"/signup"}>
+                <span>Join Dungeon Chat</span>
+              </Link>
             </div>
-          </div>
         </div>
 
-        <footer>Copyright &copy; 2019 Turd Burglers</footer>
+        <footer>
+          <span>
+            Image credits: Randall Hampton
+          </span>
+          <span>
+            Copyright &copy; 2019 aA DungeonMasters
+          </span>
+        </footer>
       </div>
     );
   }
